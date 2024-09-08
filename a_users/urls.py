af3b84bbbcbc08b8049842beb3cce3ccdc464898
mohiuddin1221 <topumohiuddin3@gmail.com>
2024-09-08 14:32:00 +0600

    ridiclious Change

--- a_users/urls.py
+++ a_users/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('emailchange/', profile_emailchange, name="profile-emailchange"),
     path('emailverify/', profile_emailverify, name="profile-emailverify"),
     path('delete/', profile_delete_view, name="profile-delete"),
+    return redirect('amar sonar bangla')
+
+    return redirect('ami tomai valobsi')
 ]
